f3f4c1fe8bb076714ba9528b7e51adf860e50b87
ken <kchiem12@gmail.com>
2022-09-09 03:59:19 -0400

    cleaned up files

--- src/components/Session/context.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const AuthorizedUserCont = React.createContext(null);
-
-export default AuthorizedUserCont;
--- src/components/Session/index.js
+++
@@ -1,5 +0,0 @@
-import AuthorizedUserCont from "./context";
-import withAuthentication from "./withAuthentication";
-
-
-export {AuthorizedUserCont, withAuthentication};
